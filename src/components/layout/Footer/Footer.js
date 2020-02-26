588e5cefc5697b2f7693b556b08cea1b728735cf
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-02-26 13:19:03 +0100

    Add footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,11 +1,94 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faPhone, faCheck, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faFacebookF, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import clsx from 'clsx';
 
 import styles from './Footer.module.scss';
 
 const Footer = () => {
   return ( 
     <footer className={styles.root}>
-      Footer
+      <section className="container">
+        <div className={styles.categories}>
+          <div className={styles.category}>
+            <h3>Contact</h3>
+            <div className={styles.category__item}>
+              <span className={styles.icon}>
+                <FontAwesomeIcon icon={faHome}/>
+              </span>
+              <div>
+                <p>
+                  XXXX Wilshire Blvd., 
+                </p>
+                <p>
+                  Beverly Hills, CA 90212, USA.
+                </p>
+              </div>
+            </div>
+            <div className={styles.category__item}>
+              <span className={styles.icon}>
+                <FontAwesomeIcon icon={faEnvelope}/>
+              </span>
+              <p>example@gmail.com</p>
+            </div>
+            <div className={styles.category__item}>
+              <span className={styles.icon}>
+                <FontAwesomeIcon icon={faPhone}/>
+              </span>
+              <p>+1XXXXXXXXXX</p>
+            </div>
+          </div>
+          <div className={styles.category}>
+            <h3>Newsletter</h3>
+            <div className={styles.category__item}>
+              <p>Every week raports about our promotions:</p>
+            </div>
+            <form className={styles.newsletter}>
+              <input 
+                className={styles.newsletter__input} 
+                type="email" 
+                placeholder="Your email address"
+              />
+              <button 
+                className={styles.newsletter__btn}
+                type="submit"
+              >
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+            </form>
+          </div>
+          <div className={styles.category}>
+            <h3>Connect with us</h3>
+            <div className={styles.social}>
+              <a 
+                className={clsx([styles.social__icon, styles.social__facebook])} 
+                href="https://facebook.com"
+                target="__blank"
+              >
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a 
+                className={clsx([styles.social__icon, styles.social__youtube])} 
+                href="https://youtube.com"
+                target="__blank"
+              >
+                <FontAwesomeIcon icon={faYoutube} />
+              </a>
+              <a 
+                className={clsx([styles.social__icon, styles.social__twitter])} 
+                href="https:/twitter.com"
+                target="__blank"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <p>&copy; 2020 created by Łukasz Wojdat</p>
     </footer>
    );
 }
