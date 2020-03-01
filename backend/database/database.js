2e3119965384a3277491d2e47dba552543b9cf8d
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-03-01 12:08:32 +0100

    Add database connection

---
+++ backend/database/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const dbUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-r6yso.mongodb.net/game-store?retryWrites=true&w=majority`;
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database connected!');
+});
--- backend/server.js
+++ backend/server.js
@@ -16,3 +16,5 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}.`);
 });
+
+require('./database/database');
