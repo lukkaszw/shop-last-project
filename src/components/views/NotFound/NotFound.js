48d931e172b9acc72a6bdd7afe36f58799664249
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-03-09 20:14:51 +0100

    Add NotFound page

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import MessageWithLink from '../../common/MessageWithLink/MessageWithLink';
 import styles from './NotFound.module.scss';
 
 const NotFound = () => {
   return ( 
-    <div>
-      <h1>Page not found!</h1>
+    <div className={styles.root}>
+      <h2>Page not found!</h2>
+      <div className={styles.link}>
+        <MessageWithLink 
+          message="Please back to shop:"
+        />
+      </div>
     </div> 
   );
 }
